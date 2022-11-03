35a99814d629d18cd410f8bde5db9e4b0d00d84e
numb0y <lucaspatriota@outlook.com>
2022-11-03 18:01:26 -0300

    alterações na aplicação

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 
 import Home from './pages/Home'
 import Contato from './pages/contato'
