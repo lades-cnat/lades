566ad6c8c7327b017b71ff318942f0fb3b0c0a7c
Leonardo Lucena <leonardo.lucena@ifrn.edu.br>
2023-02-01 09:05:50 -0300

    Update index.js

--- src/pages/Pesquisadores/index.js
+++ src/pages/Pesquisadores/index.js
@@ -36,14 +36,14 @@ function Page() {
 
 <nav className="nav">
 <ul className="ulheader">
-      <a href="/lades/#/sobre" className="nucleo">LABORATÓRIO</a>
-      <a href="/lades/#/pesquisadores" className="nucleo">PESQUISADORES</a>
-      <a href="/lades/#/pesquisa" className="nucleo">LINHAS DE PESQUISA</a>
-      <a href="/lades/#/projetos" className="nucleo">PROJETOS</a>
-      <a href="/lades/#/biblioteca" className="nucleo">BIBLIOTECA</a>
+      <a href="#/sobre" className="nucleo">LABORATÓRIO</a>
+      <a href="#/pesquisadores" className="nucleo">PESQUISADORES</a>
+      <a href="#/pesquisa" className="nucleo">LINHAS DE PESQUISA</a>
+      <a href="#/projetos" className="nucleo">PROJETOS</a>
+      <a href="#/biblioteca" className="nucleo">BIBLIOTECA</a>
 
 <ul className="ulcontato">
-<form action="/lades/#/contato">
+<form action="#/contato">
         <button type="submit" className="contato"></button>
     </form>
 </ul>
