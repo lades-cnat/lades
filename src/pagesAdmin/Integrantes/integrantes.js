e65db80ecf287c75f91d71bd916363f9d152d69e
JOSE JEFFERSON MATIAS LEITE <matias.jefferson@academico.ifrn.edu.br>
2023-07-25 14:47:01 -0300

    Edit, Remove e adição de imagem do integrante

--- src/pagesAdmin/Integrantes/integrantes.js
+++ src/pagesAdmin/Integrantes/integrantes.js
@@ -4,13 +4,13 @@ import Footer from '../../components/footer';
 import '../Pesquisas/pesquisas.css';
 import DetalhesIntegrantes from './detalhes';
 
-
 function Integrantes() {
   const [integrantes, setIntegrantes] = useState([]);
   const [nome, setNome] = useState('');
   const [curriculo, setCurriculo] = useState('');
   const [email, setEmail] = useState('');
   const [papel, setPapel] = useState('');
+  const [imagem, setImagem] = useState(null);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -20,13 +20,32 @@ function Integrantes() {
       curriculo: curriculo,
       email: email,
       papel: papel,
+      imagem: imagem, // Incluímos a imagem aqui
     };
     setIntegrantes([...integrantes, novoIntegrante]);
-    console.log('Dados do formulário de integrante:', { nome, curriculo, email, papel });
+    console.log('Dados do formulário de integrante:', { nome, curriculo, email, papel, imagem });
+    // Limpamos os campos após o registro
+    setNome('');
+    setCurriculo('');
+    setEmail('');
+    setPapel('');
+    setImagem(null);
   };
 
   const handleDetalharIntegrante = (id) => {
     console.log('Detalhar integrante com id:', id);
+  };
+
+  const handleRemoverIntegrante = (id) => {
+    const novosIntegrantes = integrantes.filter((integrante) => integrante.id !== id);
+    setIntegrantes(novosIntegrantes);
+  };
+
+  const handleEditarIntegrante = (id, novoIntegrante) => {
+    const novosIntegrantes = integrantes.map((integrante) =>
+      integrante.id === id ? { ...integrante, ...novoIntegrante } : integrante
+    );
+    setIntegrantes(novosIntegrantes);
   };
 
   return (
@@ -67,7 +86,6 @@ function Integrantes() {
           <div>
             <label htmlFor="papel">Papel: </label>
             <select id="papel" value={papel} onChange={(e) => setPapel(e.target.value)} required>
-              <option value="">Selecione um papel</option>
               <option value="pesquisador">Pesquisador</option>
               <option value="colaborador">Colaborador</option>
               <option value="técnico">Técnico</option>
@@ -75,11 +93,25 @@ function Integrantes() {
               <option value="lider">Líder</option>
             </select>
           </div>
+          <div>
+            <label htmlFor="imagem">Imagem do Integrante: </label>
+            <input
+              type="file"
+              id="imagem"
+              onChange={(e) => setImagem(e.target.files[0])}
+            />
+          </div>
           <button type="submit">Registrar</button>
         </form>
       </main>
       <aside>
-        <DetalhesIntegrantes integrantes={integrantes} onDetalhar={handleDetalharIntegrante} />
+        <DetalhesIntegrantes
+          integrantes={integrantes}
+          onDetalhar={handleDetalharIntegrante}
+          onRemover={handleRemoverIntegrante}
+          onEditar={handleEditarIntegrante}
+          setIntegrantes={setIntegrantes}
+        />
       </aside>
       <Footer />
     </div>
